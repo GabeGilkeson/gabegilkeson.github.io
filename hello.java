1e468b03f29691b24cf279afac6b1fab9becd48c
GabeGilkeson <143174159+GabeGilkeson@users.noreply.github.com>
2024-09-13 21:37:52 -0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,27 @@
+/*
+Written by Gabe Gilkeson
+For Mr. Gross's Software Developement class
+
+Description: This program inputs the names of my family memebers
+*/
+public class hello {
+
+    public static void main(String[] args) {
+        String myname = "Gabe";
+        int myage = 17;
+
+        String momsname = "Christie";
+
+        String dadsname = "Greg";
+
+        String sistersname = "Gracie";
+
+        System.out.println("Hello World");
+        System.out.println();
+        System.out.println("My name is " + myname + ", and I am " + myage + " years old.");
+        System.out.println();
+        System.out.println("My Mom's name is " + momsname + " and my Dad's name is " + dadsname + ".");
+        System.out.println();
+        System.out.print("My sister's name is " + sistersname + ".");
+    }
+}
