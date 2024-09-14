f78b4d4529f733a3f0a32e3193f6c821b6fd1387
GabeGilkeson <143174159+GabeGilkeson@users.noreply.github.com>
2024-09-13 21:41:50 -0700

    flowcontrol.java

--- flowcontrol.java
+++ flowcontrol.java
@@ -1,3 +1,9 @@
+/*
+Written by Gabe Gilkeson
+For Mr. Gross's Software Development class
+
+Description: This program counts from 1 to 100 and determines if the numbers are even or odd. It then adds the sum of the numbers.
+*/
 public class flowcontrol {
     public static void main(String[] args) {
         int sum = 0;
