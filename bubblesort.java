6be61042dcacc77d52f50b743ed9b7fbd53b03af
GabeGilkeson <143174159+GabeGilkeson@users.noreply.github.com>
2024-09-23 06:57:13 -0700

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,44 @@
+public class bubblesort {
+
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        int lastUnsorted = array.length - 1;
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < lastUnsorted; i++) {
+                if (array[i] > array[i + 1]) {
+                    array = swapTwoArrayElements(array, i);
+                    isSorted = false;
+                }
+            }
+            lastUnsorted--;
+        }
+    }
+
+    public static int[] swapTwoArrayElements(int[] arrayToSwap, int lowerIndex) {
+        int temp;
+        temp = arrayToSwap[lowerIndex];
+        arrayToSwap[lowerIndex] = arrayToSwap[lowerIndex + 1];
+        arrayToSwap[lowerIndex + 1] = temp;
+        return arrayToSwap;
+    }
+
+    public static void main(String[] args) {
+        int[] array={1,3,4,5,1,23,57,126,4,543,345,23,12,45,67,97};
+
+        System.out.print("Unsorted array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
+        bubbleSort(array);
+
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
