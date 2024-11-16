3268cb82e88bd2958609865a4b5fa10e209fe4da
GabeGilkeson <143174159+GabeGilkeson@users.noreply.github.com>
2024-11-15 19:52:58 -0800

    Add files via upload

---
+++ Horse/HorseList.java
@@ -0,0 +1,30 @@
+/*
+Written by Gabe Gilkeson
+For Mr. Gross's Software Development class
+
+Description: This program manages a list of Horse objects, creates a few instances of horses,
+and calculates their ages.
+*/
+
+package Horse;
+
+import java.util.ArrayList;
+
+public class HorseList {
+    public static void main(String[] args) {
+        ArrayList<Horse> objects = new ArrayList<Horse>();
+
+        Horse horse1 = new Horse("Trigger", 2000);
+        Horse horse2 = new Horse("Tonto", 2001);
+        Horse horse3 = new Horse("Holler", 2002);
+
+        objects.add(horse1);
+        objects.add(horse2);
+        objects.add(horse3);
+
+        for (Horse horse : objects) {
+            System.out.println(horse);
+            horse.age(2024);
+        }
+    }
+}
